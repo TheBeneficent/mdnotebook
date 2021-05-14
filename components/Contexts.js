308fd56be5c2180f4d69e84eab37f0e0156acae4
Mohsen <sajad.ghobadi@hotmail.com>
2021-05-14 21:18:22 +0430

    Revert "react navigation sucks"
    This reverts commit 078a5b63

--- components/Contexts.js
+++ components/Contexts.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 const NotesContext = React.createContext([]);
 const NotePathContext=React.createContext('');
-const ReRenderContext=React.createContext(false)
 const NotesProvider=NotesContext.Provider;
 const NotesConsumer=NotesContext.Consumer;
 const NotePathProvider=NotePathContext.Provider;
 const NotePathConsumer=NotePathContext.Consumer;
-const ReRenderProvider=ReRenderContext.Provider;
-const ReRenderConsumer=ReRenderContext.Consumer;
 
-export {NotesContext, NotePathContext, ReRenderContext, NotesProvider, NotesConsumer, NotePathProvider, NotePathConsumer, ReRenderProvider, ReRenderConsumer};
+export {NotesProvider, NotesConsumer, NotePathProvider, NotePathConsumer};
